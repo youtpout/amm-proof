673607a0339aecd96de9240cc102eaada4fd6386
eddy <eddyboughioul@hotmail.fr>
2025-06-21 16:29:52 +0200

    add coupon to smartcontract

---
+++ dex-solidity/src/Pool.sol
@@ -0,0 +1,190 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+// inspired by simple amm sample https://www.cyfrin.io/glossary/constant-product-amm-solidity-code-example
+contract Pool {
+    IERC20 public immutable token0;
+    IERC20 public immutable token1;
+
+    struct PayedFee {
+        uint256 token0Fee;
+        uint256 token1Fee;
+        bool redeemed;
+    }
+
+    uint256 public reserve0;
+    uint256 public reserve1;
+
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => uint256) public allowance;
+
+    mapping(address => PayedFee) public userPayedFee;
+    mapping(address => uint256) public reduceFee;
+
+    constructor(address _token0, address _token1) {
+        token0 = IERC20(_token0);
+        token1 = IERC20(_token1);
+    }
+
+    function _mint(address _to, uint256 _amount) private {
+        balanceOf[_to] += _amount;
+        totalSupply += _amount;
+    }
+
+    function _burn(address _from, uint256 _amount) private {
+        balanceOf[_from] -= _amount;
+        totalSupply -= _amount;
+    }
+
+    function _update(uint256 _reserve0, uint256 _reserve1) private {
+        reserve0 = _reserve0;
+        reserve1 = _reserve1;
+    }
+
+    function addEternalCoupon(bytes32 coupon, address user, uint256 fee) external {
+        require(user != address(0), "invalid user");
+        require(fee > 0 && fee < 500, "invalid fee");
+        reduceFee[user] = fee;
+    }
+
+    function swap(
+        address _tokenIn,
+        uint256 _amountIn,
+        bytes32 _oneTimeCoupon
+    ) external returns (uint256 amountOut) {
+        require(
+            _tokenIn == address(token0) || _tokenIn == address(token1),
+            "invalid token"
+        );
+        require(_amountIn > 0, "amount in = 0");
+
+        bool isToken0 = _tokenIn == address(token0);
+        (
+            IERC20 tokenIn,
+            IERC20 tokenOut,
+            uint256 reserveIn,
+            uint256 reserveOut
+        ) = isToken0
+                ? (token0, token1, reserve0, reserve1)
+                : (token1, token0, reserve1, reserve0);
+
+        tokenIn.transferFrom(msg.sender, address(this), _amountIn);
+
+        // 0.5% fee
+        uint256 defaultFee = 500;
+        uint256 userFee = reduceFee[msg.sender];
+        bool isReduceFee = userFee > 0;
+        uint256 feeCalculated = isReduceFee
+            ? 100_000 - userFee
+            : 100_000 - defaultFee;
+
+        uint256 amountInWithFee = (_amountIn * feeCalculated) / 100_000;
+        uint256 feePayed = _amountIn - amountInWithFee;
+
+        if (!isReduceFee) {
+            // if they are not reduced fee we store fee to reimboursed later
+            PayedFee memory payedFee = userPayedFee[msg.sender];
+            PayedFee memory totalFeePayed = PayedFee(
+                isToken0 ? payedFee.token0Fee + feePayed : payedFee.token0Fee,
+                !isToken0 ? payedFee.token1Fee + feePayed : payedFee.token1Fee,
+                false
+            );
+            userPayedFee[msg.sender] = totalFeePayed;
+        }
+        amountOut =
+            (reserveOut * amountInWithFee) /
+            (reserveIn + amountInWithFee);
+
+        tokenOut.transfer(msg.sender, amountOut);
+
+        _update(
+            token0.balanceOf(address(this)),
+            token1.balanceOf(address(this))
+        );
+    }
+
+    function addLiquidity(
+        uint256 _amount0,
+        uint256 _amount1
+    ) external returns (uint256 shares) {
+        token0.transferFrom(msg.sender, address(this), _amount0);
+        token1.transferFrom(msg.sender, address(this), _amount1);
+
+        if (reserve0 > 0 || reserve1 > 0) {
+            require(
+                reserve0 * _amount1 == reserve1 * _amount0,
+                "x / y != dx / dy"
+            );
+        }
+
+        if (totalSupply == 0) {
+            shares = _sqrt(_amount0 * _amount1);
+        } else {
+            shares = _min(
+                (_amount0 * totalSupply) / reserve0,
+                (_amount1 * totalSupply) / reserve1
+            );
+        }
+        require(shares > 0, "shares = 0");
+        _mint(msg.sender, shares);
+
+        _update(
+            token0.balanceOf(address(this)),
+            token1.balanceOf(address(this))
+        );
+    }
+
+    function removeLiquidity(
+        uint256 _shares
+    ) external returns (uint256 amount0, uint256 amount1) {
+        uint256 bal0 = token0.balanceOf(address(this));
+        uint256 bal1 = token1.balanceOf(address(this));
+
+        amount0 = (_shares * bal0) / totalSupply;
+        amount1 = (_shares * bal1) / totalSupply;
+        require(amount0 > 0 && amount1 > 0, "amount0 or amount1 = 0");
+
+        _burn(msg.sender, _shares);
+        _update(bal0 - amount0, bal1 - amount1);
+
+        token0.transfer(msg.sender, amount0);
+        token1.transfer(msg.sender, amount1);
+    }
+
+    function _sqrt(uint256 y) private pure returns (uint256 z) {
+        if (y > 3) {
+            z = y;
+            uint256 x = y / 2 + 1;
+            while (x < z) {
+                z = x;
+                x = (y / x + x) / 2;
+            }
+        } else if (y != 0) {
+            z = 1;
+        }
+    }
+
+    function _min(uint256 x, uint256 y) private pure returns (uint256) {
+        return x <= y ? x : y;
+    }
+}
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+    function allowance(
+        address owner,
+        address spender
+    ) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+}
